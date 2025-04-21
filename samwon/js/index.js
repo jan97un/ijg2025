9c23bdb874c9597d2c0f9b26ada8661562e44d67
jan97un <jan97un@naver.com>
2025-04-21 14:48:29 +0900

    first commit

--- samwon/js/index.js
+++ samwon/js/index.js
@@ -1,8 +1,20 @@
 $(".main-visual-items").eq(0).addClass("on");
 $(".main-visual-items").eq(0).find(".visual-btn").addClass("off");
-$(".visual-btn").click(function () {
+$(".main-visual-items").eq(0).find(".item-num").addClass("off");
+function activateSlide() {
   $(".main-visual-items").removeClass("on");
   $(".visual-btn").removeClass("off");
+  $(".item-num").removeClass("off");
+}
+$(".visual-btn").click(function () {
+  activateSlide();
   $(this).closest(".main-visual-items").addClass("on");
   $(this).addClass("off");
+  $(this).siblings(".btn-inner").find(".item-num").addClass("off");
+});
+$(".btn-inner").click(function () {
+  activateSlide();
+  $(this).closest(".main-visual-items").addClass("on");
+  $(this).siblings(".visual-btn").addClass("off");
+  $(this).find(".item-num").addClass("off");
 });
