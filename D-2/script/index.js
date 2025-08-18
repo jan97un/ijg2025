324c117fbab1ad282316608c0cacc5925d36c244
jan97un <jan97un@naver.com>
2025-08-18 09:10:50 +0900

    first commit

---
+++ D-2/script/index.js
@@ -0,0 +1,34 @@
+$(".lnb").hide();
+$(".gnb>li")
+  .mouseover(function () {
+    $(this).find(".lnb").stop().slideDown(300);
+  })
+  .mouseout(function () {
+    $(this).find(".lnb").stop().slideUp(300);
+  });
+$(".lnb>li")
+  .mouseover(function () {
+    $(this).closest(".gnb>li").find("a").addClass("on");
+  })
+  .mouseout(function () {
+    $(this).closest(".gnb>li").find("a").removeClass("on");
+  });
+
+const slider = $(".slider");
+let index = 0;
+const count = slider.length;
+const interval = 3000;
+const speed = 600;
+
+slider.hide().eq(0).show();
+
+setInterval(function () {
+  const current = slider.eq(index);
+  index = (index + 1) % count;
+  const next = slider.eq(index);
+
+  next.css({ left: -next.width() }).show().animate({ left: 0 }, speed);
+  current.animate({ left: current.width() }, speed, function () {
+    $(this).hide;
+  });
+}, interval);
